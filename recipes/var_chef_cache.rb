404de9b567db9484e4d4a9c2710f1f53aec3fcda
Colin Shield <pair+cshield@pivotal.io>
2017-02-14 17:58:33 +0000

    first pass at Linux

--- recipes/var_chef_cache.rb
+++ recipes/var_chef_cache.rb
@@ -1,8 +1,8 @@
 # Solo doesn't necessarily create the file_cache_path,
 # but we don't want to mess with it if it exists
 directory Chef::Config[:file_cache_path] do
-  owner 'root'
-  group 'admin'
+  owner node['sprout']['cache_owner']['user']
+  group node['sprout']['cache_owner']['group']
   mode 0777
   action :create
   recursive true
